70e09f6a8e0ec7947ea4fa0fb9f5ad65c27ec605
darshan1059 <chandradarshanagrawal1059@gmail.com>
2021-11-16 19:46:44 +0530

    Create assignment1.js
    done

--- Nodejs/assignment1.js
+++ Nodejs/assignment1.js
@@ -1,19 +1,18 @@
-function greet(){
+exports.greets = function(){
+
     let n = new Date();
+
     let givenTime = n.getHours();
-    if(givenTime>5 && givenTime<12)
+
+    if (givenTime > 5 && givenTime < 12) 
     {
         console.log("Good Morning..!");
-    }
-
-    else if(givenTime>12 && givenTime<17)
-    {
+    } 
+    else if (givenTime > 12 && givenTime < 17) {
         console.log("Good Afternoon");
-    }
-    else{
+    } 
+    else {
         console.log("Good evening");
     }
 
 }
-
-greet();
